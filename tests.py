ed10ff7f04efa26e939d745d496291af5a5b7c1d
Gabriel Falcao <gabriel@nacaolivre.org>
2012-07-30 23:18:52 -0400

    less unicode

--- tests.py
+++ tests.py
@@ -34,7 +34,7 @@ def test_vargas_should_ter_um_numero_de_versao():
     assert that(vargas.version).equals("0.1")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_menos_de_1_minuto_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha menos de 1 minuto atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha menos de 1 minuto atras"
     relative_time = datetime.now() - timedelta(seconds=55)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há menos de um minuto")
@@ -42,7 +42,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_menos_de_1_m
     assert that(unicode(decorrido)).equals(u"há menos de um minuto")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_minuto_e_meio_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 minuto e meio atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 minuto e meio atras"
     relative_time = datetime.now() - timedelta(seconds=90)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há pouco mais de 1 minuto")
@@ -50,7 +50,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_minuto_e_m
     assert that(unicode(decorrido)).equals(u"há pouco mais de 1 minuto")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_minuto_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 minuto atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 minuto atras"
     relative_time = datetime.now() - timedelta(minutes=1)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 minuto")
@@ -58,7 +58,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_minuto_atr
     assert that(unicode(decorrido)).equals(u"há 1 minuto")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_minutos_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 4 minutos atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 4 minutos atras"
     relative_time = datetime.now() - timedelta(minutes=4)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 4 minutos")
@@ -66,7 +66,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_minutos_at
     assert that(unicode(decorrido)).equals(u"há 4 minutos")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_55_minutos_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 55 minutos atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 55 minutos atras"
     relative_time = datetime.now() - timedelta(minutes=55)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 55 minutos")
@@ -74,7 +74,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_55_minutos_a
     assert that(unicode(decorrido)).equals(u"há 55 minutos")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_hora_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 hora atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 hora atras"
     relative_time = datetime.now() - timedelta(minutes=60)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 hora")
@@ -82,7 +82,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_hora_atras
     assert that(unicode(decorrido)).equals(u"há 1 hora")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_horas_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 4 horas atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 4 horas atras"
     relative_time = datetime.now() - timedelta(minutes=60 * 4)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 4 horas")
@@ -90,7 +90,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_horas_atra
     assert that(unicode(decorrido)).equals(u"há 4 horas")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_dia_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 dia atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 dia atras"
     relative_time = datetime.now() - timedelta(minutes=60 * 24)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 dia")
@@ -98,7 +98,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_dia_atras(
     assert that(unicode(decorrido)).equals(u"há 1 dia")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_dias_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 4 dias atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 4 dias atras"
     relative_time = datetime.now() - timedelta(minutes=(60 * 24) * 4)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 4 dias")
@@ -106,7 +106,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_dias_atras
     assert that(unicode(decorrido)).equals(u"há 4 dias")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_hora_e_24_minutos_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 hora e 24 minutos atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 hora e 24 minutos atras"
     relative_time = datetime.now() - timedelta(minutes=84)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 hora e 24 minutos")
@@ -114,7 +114,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_hora_e_24_
     assert that(unicode(decorrido)).equals(u"há 1 hora e 24 minutos")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_horas_atras_3min():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 4 horas e e minutos atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 4 horas e e minutos atras"
     relative_time = datetime.now() - timedelta(minutes=(60 * 4) + 3)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 4 horas e 3 minutos")
@@ -122,7 +122,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_horas_atra
     assert that(unicode(decorrido)).equals(u"há 4 horas e 3 minutos")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_pouco_mais_de_1_hora_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 hora atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 hora atras"
     relative_time = datetime.now() - timedelta(minutes=61)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há pouco mais de 1 hora")
@@ -130,7 +130,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_pouco_mais_d
     assert that(unicode(decorrido)).equals(u"há pouco mais de 1 hora")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_hora_e_meia():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 hora atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 hora atras"
     relative_time = datetime.now() - timedelta(minutes=90)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 hora e meia")
@@ -138,7 +138,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_hora_e_mei
     assert that(unicode(decorrido)).equals(u"há 1 hora e meia")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_semana_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 semana atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 semana atras"
     relative_time = datetime.now() - timedelta(minutes=60 * 24 * 7)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 semana")
@@ -146,7 +146,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_semana_atr
     assert that(unicode(decorrido)).equals(u"há 1 semana")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_semanas_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 4 semanas atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 4 semanas atras"
     relative_time = datetime.now() - timedelta(minutes=(60 * 24 * 7) * 4)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 4 semanas")
@@ -154,7 +154,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_semanas_at
     assert that(unicode(decorrido)).equals(u"há 4 semanas")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_mes_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 mês atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 mês atras"
     relative_time = datetime.now() - timedelta(minutes=60 * 24 * 30)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 mês")
@@ -162,7 +162,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_mes_atras(
     assert that(unicode(decorrido)).equals(u"há 1 mês")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_meses_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 4 meses atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 4 meses atras"
     relative_time = datetime.now() - timedelta(minutes=(60 * 24 * 30) * 4)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 4 meses")
@@ -170,7 +170,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_meses_atra
     assert that(unicode(decorrido)).equals(u"há 4 meses")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_mes_atras_com_31_dias():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 mês atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 mês atras"
     relative_time = datetime.now() - timedelta(minutes=60 * 24 * 31)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 mês")
@@ -178,7 +178,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_mes_atras_
     assert that(unicode(decorrido)).equals(u"há 1 mês")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_mess_atras_com_31_dias():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 4 meses atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 4 meses atras"
     relative_time = datetime.now() - timedelta(minutes=(60 * 24 * 31) * 4)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 4 meses")
@@ -186,7 +186,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_mess_atras
     assert that(unicode(decorrido)).equals(u"há 4 meses")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_ano_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 1 ano atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 1 ano atras"
     relative_time = datetime.now() - timedelta(minutes=60 * 24 * 365)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 1 ano")
@@ -194,7 +194,7 @@ def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_1_ano_atras(
     assert that(unicode(decorrido)).equals(u"há 1 ano")
 
 def test_tempo_should_descrever_quando_difereca_de_tempo_ocorreu_ha_4_anos_atras():
-    u"TempoRelativo deveria descrever quando difereça de tempo ocorreu ha 4 anos atras"
+    u"TempoRelativo deveria descrever quando diferenca de tempo ocorreu ha 4 anos atras"
     relative_time = datetime.now() - timedelta(minutes=(60 * 24 * 365) * 4)
     decorrido = TempoRelativo(relative_time)
     assert that(decorrido.ha).equals(u"há 4 anos")
